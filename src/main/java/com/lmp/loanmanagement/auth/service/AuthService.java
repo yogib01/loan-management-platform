2c33d05594b10fbcbd73604898f0c9ef62201130
yogib01 <yogeshbawaskar01@gmail.com>
2025-12-22 19:29:09 +0530

    authentication service for login added

--- src/main/java/com/lmp/loanmanagement/auth/service/AuthService.java
+++ src/main/java/com/lmp/loanmanagement/auth/service/AuthService.java
@@ -1,5 +1,39 @@
 package com.lmp.loanmanagement.auth.service;
 
+import com.lmp.loanmanagement.auth.entity.User;
+import com.lmp.loanmanagement.auth.repository.UserRepository;
+import com.lmp.loanmanagement.config.security.JwtTokenProvider;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
 public class AuthService {
 
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtTokenProvider jwtTokenProvider;
+
+    public AuthService(UserRepository userRepository,
+                       PasswordEncoder passwordEncoder,
+                       JwtTokenProvider jwtTokenProvider) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+        this.jwtTokenProvider = jwtTokenProvider;
+    }
+
+    public String login(String username, String password) {
+
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() ->
+                        new RuntimeException("Invalid username or password"));
+
+        if (!passwordEncoder.matches(password, user.getPassword())) {
+            throw new RuntimeException("Invalid username or password");
+        }
+
+        return jwtTokenProvider.generateToken(
+                user.getUsername(),
+                user.getRole().name()
+        );
+    }
 }
