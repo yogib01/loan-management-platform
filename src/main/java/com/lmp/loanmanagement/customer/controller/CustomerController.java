395a960a83132448d7df82b010f17e2c55001233
yogib01 <yogeshbawaskar01@gmail.com>
2025-12-22 19:54:24 +0530

    CustomerService added

--- src/main/java/com/lmp/loanmanagement/customer/controller/CustomerController.java
+++ src/main/java/com/lmp/loanmanagement/customer/controller/CustomerController.java
@@ -1,5 +1,43 @@
-package com.lmp.loanmanagement.customer.controller;
+package com.lmp.loanmanagement.customer.service;
 
-public class CustomerController {
+import com.lmp.loanmanagement.customer.entity.Customer;
+import com.lmp.loanmanagement.customer.repository.CustomerRepository;
+import org.springframework.stereotype.Service;
 
+import java.util.List;
+
+@Service
+public class CustomerService {
+
+    private final CustomerRepository customerRepository;
+
+    public CustomerService(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
+
+    public Customer createCustomer(Customer customer) {
+        return customerRepository.save(customer);
+    }
+
+    public Customer getCustomerById(Long id) {
+        return customerRepository.findById(id)
+                .orElseThrow(() ->
+                        new RuntimeException("Customer not found with id: " + id));
+    }
+
+    public List<Customer> getAllCustomers() {
+        return customerRepository.findAll();
+    }
+
+    public Customer updateCustomer(Long id, Customer updatedCustomer) {
+
+        Customer existingCustomer = getCustomerById(id);
+
+        existingCustomer.setFullName(updatedCustomer.getFullName());
+        existingCustomer.setMonthlyIncome(updatedCustomer.getMonthlyIncome());
+        existingCustomer.setEmploymentType(updatedCustomer.getEmploymentType());
+        existingCustomer.setActive(updatedCustomer.isActive());
+
+        return customerRepository.save(existingCustomer);
+    }
 }
