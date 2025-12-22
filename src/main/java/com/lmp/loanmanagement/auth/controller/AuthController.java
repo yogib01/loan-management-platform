d79ceac89b562ca4af4e629be009e806b332309f
yogib01 <yogeshbawaskar01@gmail.com>
2025-12-22 19:32:13 +0530

    auth controller with login API added

--- src/main/java/com/lmp/loanmanagement/auth/controller/AuthController.java
+++ src/main/java/com/lmp/loanmanagement/auth/controller/AuthController.java
@@ -1,5 +1,29 @@
 package com.lmp.loanmanagement.auth.controller;
 
+import com.lmp.loanmanagement.auth.dto.AuthResponse;
+import com.lmp.loanmanagement.auth.dto.LoginRequest;
+import com.lmp.loanmanagement.auth.service.AuthService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/auth")
 public class AuthController {
 
+    private final AuthService authService;
+
+    public AuthController(AuthService authService) {
+        this.authService = authService;
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<AuthResponse> login(@RequestBody LoginRequest request) {
+
+        String token = authService.login(
+                request.getUsername(),
+                request.getPassword()
+        );
+
+        return ResponseEntity.ok(new AuthResponse(token));
+    }
 }
