b37f92b92e5db3334da30ddbfc25b1dc1d3a233c
yogib01 <yogeshbawaskar01@gmail.com>
2025-12-22 19:26:59 +0530

    login request and auth response DTOs added

--- src/main/java/com/lmp/loanmanagement/auth/dto/AuthResponse.java
+++ src/main/java/com/lmp/loanmanagement/auth/dto/AuthResponse.java
@@ -2,4 +2,13 @@ package com.lmp.loanmanagement.auth.dto;
 
 public class AuthResponse {
 
+    private String token;
+
+    public AuthResponse(String token) {
+        this.token = token;
+    }
+
+    public String getToken() {
+        return token;
+    }
 }
