9fa74b003c18a1685d19a03a4d9b519247bba081
yogib01 <yogeshbawaskar01@gmail.com>
2025-12-22 19:16:52 +0530

    CustomUserDetailsService created

--- src/main/java/com/lmp/loanmanagement/config/security/CustomUserDetailsService.java
+++ src/main/java/com/lmp/loanmanagement/config/security/CustomUserDetailsService.java
@@ -1,5 +1,34 @@
 package com.lmp.loanmanagement.config.security;
 
-public class CustomUserDetailsService {
+import com.lmp.loanmanagement.auth.entity.User;
+import com.lmp.loanmanagement.auth.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public CustomUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username)
+            throws UsernameNotFoundException {
+
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() ->
+                        new UsernameNotFoundException("User not found: " + username));
+
+        return org.springframework.security.core.userdetails.User
+                .withUsername(user.getUsername())
+                .password(user.getPassword())
+                .roles(user.getRole().name())
+                .disabled(!user.isActive())
+                .build();
+    }
 }
