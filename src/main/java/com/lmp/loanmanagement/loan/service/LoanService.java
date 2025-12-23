7ebdfa02376eea6fdce80f26bfa27f0f5a3ea762
yogib01 <yogeshbawaskar01@gmail.com>
2025-12-23 12:59:09 +0530

    LoanService added

--- src/main/java/com/lmp/loanmanagement/loan/service/LoanService.java
+++ src/main/java/com/lmp/loanmanagement/loan/service/LoanService.java
@@ -1,5 +1,41 @@
 package com.lmp.loanmanagement.loan.service;
 
+import com.lmp.loanmanagement.common.enums.LoanStatus;
+import com.lmp.loanmanagement.loan.entity.Loan;
+import com.lmp.loanmanagement.loan.repository.LoanRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class LoanService {
 
+    private final LoanRepository loanRepository;
+
+    public LoanService(LoanRepository loanRepository) {
+        this.loanRepository = loanRepository;
+    }
+
+    public Loan applyLoan(Loan loan) {
+        loan.setStatus(LoanStatus.APPLIED);
+        return loanRepository.save(loan);
+    }
+
+    public Loan getLoanById(Long id) {
+        return loanRepository.findById(id)
+                .orElseThrow(() ->
+                        new RuntimeException("Loan not found with id: " + id));
+    }
+
+    public List<Loan> getLoansByCustomerId(Long customerId) {
+        return loanRepository.findByCustomerId(customerId);
+    }
+
+    public Loan updateLoanStatus(Long loanId, LoanStatus status) {
+
+        Loan loan = getLoanById(loanId);
+        loan.setStatus(status);
+
+        return loanRepository.save(loan);
+    }
 }
