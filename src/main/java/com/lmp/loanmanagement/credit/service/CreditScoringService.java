cead55fa43ff6a8050e2daafb23274ce2ed71b49
yogib01 <yogeshbawaskar01@gmail.com>
2025-12-23 13:18:52 +0530

    credit scoring service with mock CIBIL integration added

--- src/main/java/com/lmp/loanmanagement/credit/service/CreditScoringService.java
+++ src/main/java/com/lmp/loanmanagement/credit/service/CreditScoringService.java
@@ -1,5 +1,35 @@
 package com.lmp.loanmanagement.credit.service;
 
+import com.lmp.loanmanagement.common.enums.RiskLevel;
+import com.lmp.loanmanagement.credit.entity.CreditScore;
+import com.lmp.loanmanagement.credit.integration.MockCibilClient;
+import org.springframework.stereotype.Service;
+
+@Service
 public class CreditScoringService {
 
+    private final MockCibilClient mockCibilClient;
+
+    public CreditScoringService(MockCibilClient mockCibilClient) {
+        this.mockCibilClient = mockCibilClient;
+    }
+
+    public CreditScore evaluateCredit(Long customerId, String panNumber) {
+
+        int score = mockCibilClient.fetchCreditScore(panNumber);
+        RiskLevel riskLevel = determineRiskLevel(score);
+
+        return new CreditScore(customerId, score, riskLevel);
+    }
+
+    private RiskLevel determineRiskLevel(int score) {
+
+        if (score >= 750) {
+            return RiskLevel.LOW;
+        } else if (score >= 600) {
+            return RiskLevel.MEDIUM;
+        } else {
+            return RiskLevel.HIGH;
+        }
+    }
 }
